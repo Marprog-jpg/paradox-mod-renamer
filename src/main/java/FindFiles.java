911114628d7916899dde013633fab224f76653bc
Marprog-jpg <78869306+Marprog-jpg@users.noreply.github.com>
2021-05-31 07:37:48 +0200

    Renamed a File because it kept deleting itself for some reason.

--- src/main/java/FindFiles.java
+++ src/main/java/FindFiles.java
@@ -16,6 +16,7 @@ public class FindFiles{
         modFiles = Main.modDir.listFiles(new FilenameFilter() {
             public boolean accept(File dir, String name) {
                 return name.toLowerCase().endsWith(".mod");
+                
             }
         });
     }
